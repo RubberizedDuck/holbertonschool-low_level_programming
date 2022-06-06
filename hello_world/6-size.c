66a4e591bf7be5186c66ea3bab88ed6627caf362
root <5051@holbertonschool.com>
2022-06-05 19:10:37 -0700

    Added "unassigned long" to printf.

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,9 +10,9 @@ int main(void)
   long int c;
   long long int d;
   float e;
- printf("Size of a char: %lu byte(s)\n", sizeof(a);
- printf("Size of an int: %lu byte(s)\n", sizeof(b));
- printf("Size of a long int: %lu byte(s)\n", sizeof(c));
- printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
- printf("Size of a float: %lu byte(s)\n", sizeof(e);
-return (0);
+  printf("Size of a char: %lu byte(s)\n", (unassigned long)sizeof(a));
+  printf("Size of an int: %lu byte(s)\n", (unassigned long)sizeof(b));
+  printf("Size of a long int: %lu byte(s)\n", (unassigned long)sizeof(c));
+  printf("Size of a long long int: %lu byte(s)\n", (unassigned long)sizeof(d));
+  printf("Size of a float: %lu byte(s)\n", (unassigned long)sizeof(e);
+	 return (0);
