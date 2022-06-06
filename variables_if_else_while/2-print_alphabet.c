b30984411052934e2b57c7c52554b96ba67ff118
root <5051@holbertonschool.com>
2022-06-05 22:13:28 -0700

    Prints the alphabet  using while loop.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Prints the alphatbet us while loop
+*Return: Always returns 0
+*/
+int main(void)
+{
+char alph;
+while (alph <= 'z')
+putchar(x);
+x++;
+return (0);
+}
