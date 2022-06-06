e03404485aa7595f56b99f5169a4a4f97d383eb5
root <5051@holbertonschool.com>
2022-06-05 19:06:12 -0700

    Prints out size of various types.

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
- char schar = ("Size of a char: 1 byte(s)");
- char sint = ("Size of an int: 4 byte(s)");
- char sintl = ("Size of a long int: 4 byte(s)");
- char sintll = ("Size of a long long int: 8 byte(s)");
- char sfloat = ("Size of a float: 4 byte(s)");
- printf("%c\n", schar);
- printf("%c\n", sint);
- printf("%c\n", sintl);
- printf("%c\n", sintll);
- printf("%c\n", sfloat);
- return (0);
+  char a;
+  int b;
+  long int c;
+  long long int d;
+  float e;
+ printf("Size of a char: %lu byte(s)\n", sizeof(a);
+ printf("Size of an int: %lu byte(s)\n", sizeof(b));
+ printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+ printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+ printf("Size of a float: %lu byte(s)\n", sizeof(e);
+return (0);
