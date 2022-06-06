1c067e42a141d8bf9a175344ee58f373d159770e
root <5051@holbertonschool.com>
2022-06-05 21:05:51 -0700

    States whether number is positive or negative.

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - function to check whether 'n' is positive
+* or negative
+*
+*Return: always returns 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	  {
+	    printf("%d is positive");
+	  } else if (n < 0)
+	  {
+	    printf("%d is negative");
+	  } else
+	  {
+	    printf("%d is zero");
+	  }
+	return (0);
+}
