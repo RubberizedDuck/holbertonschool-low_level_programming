037fce6461d383a43621592c1be9d827da6d3390
root <5051@holbertonschool.com>
2022-06-05 22:00:10 -0700

    Testing betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,9 +16,7 @@ if (lastdig > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
 }
-else if (lastdig < 6)
-&&
-(lastdig != 0)
+else if (lastdig < 6)&&(lastdig != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
 }
