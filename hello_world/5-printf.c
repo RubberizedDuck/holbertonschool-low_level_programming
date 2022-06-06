09346b5cb32c62dd7ae3e5786fbeeb713c39d996
root <5051@holbertonschool.com>
2022-06-05 18:21:42 -0700

    Uses printf to pring string

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+*main - Uses printf to print a string
+*Return: returns value
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
