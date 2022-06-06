ed0cceba2526c97865f6c726e6327649a4ef7463
root <5051@holbertonschool.com>
2022-06-05 18:39:21 -0700

    Prints out set variables.

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+*main - stores variables and prints strings of variables
+*Return: returns value
+*/
+int main(void)
+{
+ char schar = ("Size of a char: 1 byte(s)");
+ char sint = ("Size of an int: 4 byte(s)");
+ char sintl = ("Size of a long int: 4 byte(s)");
+ char sintll = ("Size of a long long int: 8 byte(s)");
+ char sfloat = ("Size of a float: 4 byte(s)");
+ printf("%c\n", schar);
+ printf("%c\n", sint);
+ printf("%c\n", sintl);
+ printf("%c\n", sintll);
+ printf("%c\n", sfloat);
+ return (0);
