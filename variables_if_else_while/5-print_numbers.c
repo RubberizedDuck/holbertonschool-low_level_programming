9e38e474bc070e44695dcacc164140f9d01615b5
root <5051@holbertonschool.com>
2022-06-05 22:57:58 -0700

    Prints from 0 to 9.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - Function to print all single digit numbers of base 10
+* starting at 0
+*Return: Always returns 0
+*/
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf(num);
+num++;
+}
+return (0);
+}
