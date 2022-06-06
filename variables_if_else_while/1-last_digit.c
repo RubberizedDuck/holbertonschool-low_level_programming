259af0ca685516d95f20d2bafb05964453b36e5d
root <5051@holbertonschool.com>
2022-06-05 21:41:33 -0700

    Added closing "}" brackets.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,8 +21,10 @@ else if (lastdig < 6)
 (lastdig != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
+}
 else
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdig);
+}
 return (0);
 }
