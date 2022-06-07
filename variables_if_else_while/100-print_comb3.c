2f949201c2e1a2dc1fae8adb1701da8676a1f82f
root <5051@holbertonschool.com>
2022-06-06 18:44:08 -0700

    Prints all possible combination of two digits.

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the possible combination of 2 digits
+ *
+ *Return: Always returns 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = '0';
+	while (a <= '8')
+	{
+		b = (a + 1);
+		while (b <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			if (a <= '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b = (b + 1);
+		}
+		a = (a + 1);
+	}
+	putchar('\n');
+	return (0);
+}
