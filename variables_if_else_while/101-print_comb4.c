1758e03967a20264e26a5d87eac0f5266c689cb8
root <5051@holbertonschool.com>
2022-06-06 18:56:18 -0700

    Prints all possible different combinations of three digits.

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the possible combination of 2 digits
+ *
+ *Return: Always returns 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	a = '0';
+	while (a < '8')
+	{
+		b = (a + 1);
+		while (b <= '9')
+		{
+			c = (b + 1);
+			while (c <= '9')
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a < '7')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+				c = (c + 1);
+			}
+			b = (b + 1);
+		}
+		a = (a + 1);
+	}
+	putchar('\n');
+	return (0);
+}
