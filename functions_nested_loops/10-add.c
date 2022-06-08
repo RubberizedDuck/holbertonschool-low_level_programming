2c463624cf3f1745b18e56dc11663b6c5220492a
root <5051@holbertonschool.com>
2022-06-07 22:43:00 -0700

    Returns the value of two integers.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - returns the value of two integers
+ *@a: first integer
+ *@b: second integer
+ * Return: returns the valu of a + b in c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
