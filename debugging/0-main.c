195d130291db51d1f6d19c8fe4bcc7a20fe8e932
root <5051@holbertonschool.com>
2022-06-09 03:28:50 -0700

    Tests if positive_or_negative works.

---
+++ debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test function "positive_or_negative" to see if function works
+ * Return: 0 if works
+ */
+
+void main(int)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n");
+	}
+}
