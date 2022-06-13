6f6c1f989603f68e1d71f8533821ff318aba37e1
root <5051@holbertonschool.com>
2022-06-12 17:01:30 -0700

    Prints numbers from 0 to 9.

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers between 0 and 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		_putchar(i);
+		i = i + 1;
+	}
+	_putchar('\n');
+}
