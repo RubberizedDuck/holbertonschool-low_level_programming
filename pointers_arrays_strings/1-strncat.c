90719d9c649fb91f024c5edf07ba3de6c08ff02e
root <5051@holbertonschool.com>
2022-06-14 17:48:31 -0700

    Changed code to create my own function.

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int i;
+
+	i = 0;
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	dest[i] = 0;
 
 	return (dest);
 }
