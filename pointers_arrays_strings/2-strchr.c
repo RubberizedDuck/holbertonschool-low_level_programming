181c82d4aa3133df2d0b5abee6509dc92ead70f3
root <5051@holbertonschool.com>
2022-06-16 18:49:07 -0700

    Function that prints the remainder of a string array from a given char of that array.

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - returns the remainder of the address of char if found
+ * @s: is the pointer to the string
+ * @c: is the starting point from the pointer
+ * Return: the remainder of the pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	/* loops while pointer doesn't equall NULL termoinator */
+	while (s[i] != '\0')
+	{
+		/* returns remainder of c if true */
+		if (c == s[i])
+		{
+			return (&s[i]);
+		}
+		i = i + 1;
+	}
+	/* returns what's stored in pointer */
+	return (s);
+}
