51b934262ddb543880aea7de2bc61da25fd86a7c
root <5051@holbertonschool.com>
2022-06-20 19:49:27 -0700

    Program to print the element of i in array argv followed by a new line.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the element of i in array argv followed by a new line
+ * @argc: The number of arguments being passed
+ * @argv: The array of arguments being passed
+ * @Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i = i + 1;
+	}
+	return (0);
+}
