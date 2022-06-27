c94f822adddce6ad559cc2aa6595ef7d74e2cdd7
root <5051@holbertonschool.com>
2022-06-26 21:34:05 -0700

    Fixed malloc to include null terminated byte.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -47,7 +47,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2 = n;
 	}
 	totalLen = len1 + len2;
-	concat = malloc(totalLen * sizeof(*concat));
+	concat = malloc(totalLen * sizeof(*concat) + 1);
 	if (concat == NULL)
 	{
 		return (NULL);
