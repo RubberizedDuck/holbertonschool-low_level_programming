0d74415e656f1de4f00f0056e1349d1d97304708
root <5051@holbertonschool.com>
2022-06-26 21:51:32 -0700

    Fixed malloc.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -47,7 +47,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2 = n;
 	}
 	totalLen = len1 + len2;
-	concat = malloc(totalLen * sizeof(*concat) + 1 + n);
+	concat = malloc((len1 + len2) * sizeof(*concat) + 1 + n);
 	if (concat == NULL)
 	{
 		return (NULL);
