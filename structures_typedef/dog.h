4df597196836b39b75608f8e45391a49d8bbd36e
root <5051@holbertonschool.com>
2022-06-28 02:35:25 -0700

    Structure for dog information.

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
