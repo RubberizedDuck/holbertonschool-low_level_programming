0faeb9e5381fa2ce55f5020c9a29ddb3c7db4673
root <5051@holbertonschool.com>
2022-06-28 17:34:31 -0700

    Added _strlen, _strdup and new_dog protos.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,5 +17,8 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
+char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
