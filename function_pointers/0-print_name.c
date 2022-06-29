12790ff97a44da3d3d8697889c10960f9555bea1
root <5051@holbertonschool.com>
2022-06-28 21:01:00 -0700

    Function that takes in a name and uses a pointer to another function.

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - will print the name that is passed
+ * @name: the name to print
+ * @f: pointer to the function to use
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL && name == NULL)
+		return;
+	f(name);
+}
