c94315b57895b93b238be5a984a3a48c9cb402eb
root <5051@holbertonschool.com>
2022-06-28 17:47:54 -0700

    Added proto free_dog.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,5 +20,6 @@ void print_dog(struct dog *d);
 int _strlen(char *s);
 char *_strdup(char *str);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
