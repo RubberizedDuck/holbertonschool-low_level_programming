349a906f7714e55bf8f79ede026d3ace24c7dc76
root <5051@holbertonschool.com>
2022-06-30 17:21:35 -0700

    The main file.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, res, (*calc)(int, int);
+	char *s;
+
+	if (argc != 4)
+		exit(0);
+
+	s = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	calc = get_op_func(s);
+	if (!calc)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	res = calc(num1, num2);
+	printf("%d\n", res);
+
+	return (0);
+}
