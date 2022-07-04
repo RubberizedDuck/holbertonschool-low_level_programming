6f96efac65ffe86fd72a8d78c16a6d31d6001b93
root <5051@holbertonschool.com>
2022-07-03 20:48:47 -0700

    Added print_all proto and a new typedef struct.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,16 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+
+typedef struct print_format
+{
+	char *symbol;
+	void (*type)(va_list arg);
+} t_pf;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
