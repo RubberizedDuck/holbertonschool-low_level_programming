13f94139fe346420f44139266a817e77f00ccb15
root <5051@holbertonschool.com>
2022-07-03 17:31:04 -0700

    Fixed separator if statement to not include sep at end of line.

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, unsigned int));
 		i++;
-		if (separator && i < n)
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
