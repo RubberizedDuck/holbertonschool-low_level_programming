96826efd7e75cfd6d907973a16f969b8851ca36e
root <5051@holbertonschool.com>
2022-07-03 17:29:52 -0700

    Fixed if statement for separator to not include if i is less than n - 1.

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		if (i < n)
+		if (i < n - 1)
 			if (separator)
 				printf("%s", separator);
 		i++;
