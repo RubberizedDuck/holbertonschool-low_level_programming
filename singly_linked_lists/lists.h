5bee4bf2d1c04bf4e8c4b590d841a1740a32f4b6
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-12 00:33:22 -0700

    Add proto add_node and add inclusions.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,5 +21,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+int _strlen(char *s);
 
 #endif /* _LISTS_H_ */
