fbebd3b3804dc79805cbf57b017771edff13fde5
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-11 23:55:08 -0700

    Prints all the elements of a list.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the number of elements of struct list
+ * @h: pointer to first struct list
+ * Return: Number of elements created from struct list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t numOfElements = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] ", h->len);
+		if (h->str == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", h->str);
+		numOfElements++;
+		h = h->next;
+	}
+	return (numOfElements);
+}
