e104731eaa64dc42c52f506252a977217be57053
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-13 16:22:25 -0700

    Deletes head node and returns value stored.

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete's the head list node and returns the value
+ * @head - pointer to the head pointer
+ * Return: Value stored in head list, or 0 if NULL
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *copy;
+	int val;
+
+	copy = *head;
+	if (copy == NULL)
+		return (0);
+
+	val = copy->n;
+	*head = copy->next;
+	free(copy);
+
+	return (val);
+}
