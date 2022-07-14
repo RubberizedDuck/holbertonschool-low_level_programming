2752f8e98d7082295868095a80a456266ea14d6b
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-13 21:45:25 -0700

    Fix comments.

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ * inser_nodeint_at_index - finds list at a specific index
+ * @head: pointer to pointer to start of the list
+ * @idx: index to node position to insert new node at
+ * @n: the data to insert into new node
+ * Return: the address of the new node or NULL if func fails
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-		unsigned int nodeCount = 0;
+	unsigned int nodeCount = 0;
 	listint_t *new = NULL;
 	listint_t *temp = NULL;
 
