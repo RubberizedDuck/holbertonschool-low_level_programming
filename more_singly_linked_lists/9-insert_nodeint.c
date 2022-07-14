fdc03fc61217f77c4a611a614e443aadb9a1a609
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-13 21:26:26 -0700

    Inserts a new node at a given position.

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+		unsigned int nodeCount = 0;
+	listint_t *new = NULL;
+	listint_t *temp = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	if (*head == NULL && idx > 0)
+		return (NULL);
+	/* Store copy of head */
+	temp = *head;
+	if (idx == 0)
+	{
+		new = malloc(sizeof(*new));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = temp;
+		*head = new;
+		return (new);
+
+	}
+	/* find where to add new node */
+	while (temp != NULL)
+	{
+		/* Adds new node once found */
+		if (nodeCount + 1 == idx)
+		{
+			new = malloc(sizeof(*new));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		temp = temp->next;
+		nodeCount++;
+	}
+
+	return (NULL);
+}
