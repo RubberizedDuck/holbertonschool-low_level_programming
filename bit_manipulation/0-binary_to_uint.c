f9eb93f8f83eeb2d604c77769203167031b002a4
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-17 17:26:44 -0700

    Fix spacing.

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while(b[index] != '\0')
+	while (b[index] != '\0')
 	{
 		if (b[index] != '0' && b[index] != '1')
 			return (0);
