a93af599e4d9d59b2d1232e09a4a7bae3b991298
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-17 17:25:15 -0700

    Add case if string is NULL.

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0, index = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while(b[index] != '\0')
 	{
 		if (b[index] != '0' && b[index] != '1')
