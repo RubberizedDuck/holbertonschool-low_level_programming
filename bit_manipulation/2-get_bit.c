6a3a18f6664607f49919e58d4080950a9d877f5d
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-17 18:33:30 -0700

    Change code to use bitwise operators.

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,14 +1,19 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
+ * get_bit - returns the value of a bit at given index
+ * @n: integer memory location being checked
+ * @index: the bit position to be checked
+ * Return: the value of bit position or -1 on an error
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 0 && n > 0)
-		return (1);
-	return (0);
+	int bit;
+
+	if (index > 32)
+		return (-1);
+
+	bit = (n >> index) & 1;
+
+	return (bit);
 }
