752e0ee4cf2213b32d94d8d64026db7db25381ed
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-15 19:42:59 -0700

    function that adds an element to the hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update an element in a hash table
+ * @ht: pointer to the hash table
+ * @key: the key to be added - cannot be empty
+ * @value: the value of the key
+ * Return: 0 on failure or 1 on success
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *copied_value;
+	unsigned long int i, index;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+
+	copied_value = strdup(value);
+	if (copied_value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	i = index;
+	while (ht->array[i])
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = copied_value;
+			return (1);
+		}
+		i++;
+	}
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+	{
+		free(copied_value);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = copied_value;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
