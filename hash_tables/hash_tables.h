5cc223a5a3c44e9f67d062474596d03a6a9b1a41
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-15 18:58:51 -0700

    Add system headers

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef _HASH_TABLES_H_
 #define _HASH_TABLES_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
