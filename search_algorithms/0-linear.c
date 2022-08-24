3e50ce17c37fcd99de5cd8791eaf056f9bbb41e4
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-23 20:01:05 -0700

    Linear search algorithm for integers

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in an array of integers
+ * using linear search
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to find
+ * Return: the value to find, otherwise will return -1 if value
+ * is not present, or the array is NULL
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked [%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
